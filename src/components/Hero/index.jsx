0556731d4cce564be3451da3254255f227dd6ad1
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2022-02-01 20:01:35 -0300

    component hero and its title on desktop and mobile

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,7 @@
+import * as S from './styles'
+
+const Hero = ({ children }) => {
+  return <S.Hero>{children}</S.Hero>
+}
+
+export default Hero
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Hero = styled.section`
+  align-items: center;
+  background-color: var(--dark-purple);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 45rem;
+  padding: 4rem 1.5rem;
+  width: 100%;
+`
