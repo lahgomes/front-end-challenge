e7f88687e788b989e1a9f752470b9cfe7ffda3a7
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2022-02-06 20:01:36 -0300

    layout trailer

--- src/components/TrailerMovie/index.jsx
+++ src/components/TrailerMovie/index.jsx
@@ -1,17 +1,15 @@
-//import * as S from './styles'
+import * as S from './styles'
 
 const TrailerMovie = () => {
   const idVideo = '14TVi51-bgc'
   return (
-    <iframe
-      width="560"
-      height="315"
+    <S.Trailer
       src={`https://www.youtube.com/embed/${idVideo}`}
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-    ></iframe>
+    />
   )
 }
 
